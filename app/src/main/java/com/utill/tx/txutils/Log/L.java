7d236f28690bb0828c8e638b6aa7efb19b1b9a0f
tx <123436059@qq.com>
2017-02-08 17:14:47 +0800

    Initial commit

---
+++ app/src/main/java/com/utill/tx/txutils/Log/L.java
@@ -0,0 +1,68 @@
+package com.utill.tx.txutils.Log;
+
+import android.util.Log;
+
+/**
+ * Created by Taxi on 2017/2/8.
+ */
+
+public class L {
+
+    public static final String TAG = "taxi";
+    public static boolean isDebug = true;
+
+    private L() {
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    public static void d(String tag, String content) {
+        if (isDebug) {
+            Log.d(tag, content);
+        }
+    }
+
+    public static void d(String content) {
+        d(TAG, content);
+    }
+
+    public static void e(String tag, String content) {
+        if (isDebug) {
+            Log.e(tag, content);
+        }
+    }
+
+    public static void e(String content) {
+        e(TAG, content);
+    }
+
+    public static void i(String tag, String content) {
+        if (isDebug) {
+            Log.i(tag, content);
+        }
+    }
+
+    public static void i(String content) {
+        i(TAG, content);
+    }
+
+    public static void v(String tag, String content) {
+        if (isDebug) {
+            Log.v(tag, content);
+        }
+    }
+
+    public static void v(String content) {
+        v(TAG, content);
+    }
+
+    public static void w(String tag, String content) {
+        if (isDebug) {
+            Log.w(tag, content);
+        }
+    }
+
+    public static void w(String content) {
+        w(TAG, content);
+    }
+
+}
